624d7299dba2490b94f9f041b66c3d560530ee69
yangxingyuan <yangxingyuan@bytedance.com>
2023-03-29 14:17:21 +0800

    fix: ci

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -6,13 +6,13 @@ import { compile } from "../index.js";
 
 describe("compile", () => {
   test("container grammer", async (t) => {
-    let { code: result } = await compile(
-      readFileSync(path.join(__dirname, "./container.md"), "utf8"),
-      "xxx.mdx",
-      true,
-      "xxx",
-      ""
-    );
+    let { code: result } = await compile({
+      value: readFileSync(path.join(__dirname, "./container.md"), "utf8"),
+      filepath: "xxx.mdx",
+      development: true,
+      root: "xxx",
+      defaultLang: "",
+    });
 
     expect(result).toMatchSnapshot();
   });
