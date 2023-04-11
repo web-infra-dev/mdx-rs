cb00c66f15375a4032b04c5b9fe1a85ca0307efe
yangxingyuan <yangxingyuan@bytedance.com>
2023-04-11 16:31:36 +0800

    feat: support img assets in normalize_link plugin

--- crates/plugin_normalize_link/src/lib.rs
+++ crates/plugin_normalize_link/src/lib.rs
@@ -1,8 +1,33 @@
+use hast;
 use std::path::Path;
 
-use hast;
-
 const PROTOCOLS: &[&str] = &["http://", "https://", "mailto:", "tel:", "javascript:", "#"];
+const TEMP_VARIBLE: &str = "image_";
+const IMAGE_EXTNAMES: &[&str] = &[".png", ".jpg", ".jpeg", ".gif", ".svg", ".webp"];
+
+fn generate_ast_import(
+  index: usize,
+  root: &String,
+  src: &String,
+  filepath: &String,
+) -> hast::MdxjsEsm {
+  let mut import_path = src.to_string();
+  if import_path.starts_with('/') {
+    import_path = format!("{}/{}{}", root, "public", import_path);
+  }
+  if import_path.starts_with(".") {
+    import_path = normalize_link(src, root, &filepath);
+  }
+  hast::MdxjsEsm {
+    value: format!(
+      "import {} from \"{}\";",
+      format!("{}{}", TEMP_VARIBLE, index),
+      import_path
+    ),
+    position: None,
+    stops: vec![],
+  }
+}
 
 fn normalize_link(url: &String, root: &String, filepath: &String) -> String {
   // If url includes following case, return directly
@@ -47,8 +72,8 @@ fn normalize_link(url: &String, root: &String, filepath: &String) -> String {
       None => "".to_string(),
     };
 
-    // remove extname
-    if !extname.is_empty() {
+    // remove extname if it is not an image
+    if !extname.is_empty() && !IMAGE_EXTNAMES.contains(&extname.as_str()) {
       url = url.replace(&extname, "");
     }
   }
@@ -60,12 +85,15 @@ fn mdx_plugin_normalize_link_impl(
   node: &mut hast::Node,
   root: &String,
   filepath: &String,
+  images: &mut Vec<hast::MdxjsEsm>,
 ) -> Vec<String> {
   let mut links = vec![];
   match node {
     hast::Node::Root(root_node) => {
       for child in root_node.children.iter_mut() {
-        links.append(&mut mdx_plugin_normalize_link_impl(child, root, filepath));
+        links.append(&mut mdx_plugin_normalize_link_impl(
+          child, root, filepath, images,
+        ));
       }
     }
     hast::Node::Element(element) => {
@@ -86,8 +114,82 @@ fn mdx_plugin_normalize_link_impl(
           }
         }
       }
-      for child in element.children.iter_mut() {
-        links.append(&mut mdx_plugin_normalize_link_impl(child, root, filepath));
+
+      if element.tag_name == "img" {
+        // Get the src and alt property
+        let src = element.properties.iter().find(|(key, _)| key == "src");
+        let alt = element.properties.iter().find(|(key, _)| key == "alt");
+        // Then we will generate a mdxjsEsm node to import the image and push it into images
+        if let Some(src) = src {
+          if let (_, hast::PropertyValue::String(src)) = src {
+            let index = images.len();
+            images.push(generate_ast_import(index, root, src, filepath));
+            // Here we have to transform the element type to MdxJsxElement instead of replace src property
+            // because the hast parser will parse the src property as hast::PropertyValue::String
+            // and we can't get the original value
+            let new_node = hast::Node::MdxJsxElement(hast::MdxJsxElement {
+              name: Some("img".to_string()),
+              attributes: vec![
+                hast::AttributeContent::Property(hast::MdxJsxAttribute {
+                  name: "src".to_string(),
+                  value: Some(hast::AttributeValue::Expression(
+                    markdown::mdast::AttributeValueExpression {
+                      value: format!("{}{}", TEMP_VARIBLE, index),
+                      stops: vec![],
+                    },
+                  )),
+                }),
+                hast::AttributeContent::Property(hast::MdxJsxAttribute {
+                  name: "alt".to_string(),
+                  value: alt.map(|(_, value)| match value {
+                    hast::PropertyValue::String(v) => hast::AttributeValue::Literal(v.to_string()),
+                    _ => hast::AttributeValue::Literal("".to_string()),
+                  }),
+                }),
+              ],
+              children: element.children.clone(),
+              position: None,
+            });
+
+            *node = new_node;
+          }
+        }
+      }
+
+      if let Some(children) = node.children_mut() {
+        for child in children {
+          links.append(&mut mdx_plugin_normalize_link_impl(
+            child, root, filepath, images,
+          ));
+        }
+      }
+    }
+    hast::Node::MdxJsxElement(element) => {
+      if let Some(name) = &element.name {
+        if name != "img" {
+          return links;
+        }
+        // Get the src property
+        let src: Option<&mut hast::AttributeContent> =
+          element.attributes.iter_mut().find(|attr| match attr {
+            hast::AttributeContent::Property(property) => property.name == "src",
+            _ => false,
+          });
+        // Add import statement and replace the src property
+        if let Some(src) = src {
+          if let hast::AttributeContent::Property(property) = src {
+            if let Some(hast::AttributeValue::Literal(value)) = &mut property.value {
+              let index = images.len();
+              images.push(generate_ast_import(index, root, value, filepath));
+              property.value = Some(hast::AttributeValue::Expression(
+                markdown::mdast::AttributeValueExpression {
+                  value: format!("{}{}", TEMP_VARIBLE, index),
+                  stops: vec![],
+                },
+              ));
+            }
+          }
+        }
       }
     }
     _ => {}
@@ -100,7 +202,15 @@ pub fn mdx_plugin_normalize_link(
   root: &String,
   filepath: &String,
 ) -> Vec<String> {
-  mdx_plugin_normalize_link_impl(node, root, filepath)
+  let mut images: Vec<hast::MdxjsEsm> = vec![];
+  let links = mdx_plugin_normalize_link_impl(node, root, filepath, &mut images);
+  if let hast::Node::Root(root) = node {
+    // insert the images into the front of root node children
+    for image in images {
+      root.children.insert(0, hast::Node::MdxjsEsm(image));
+    }
+  }
+  links
 }
 
 #[cfg(test)]
@@ -135,5 +245,75 @@ mod tests {
       normalize_link(&"#aaa".to_string(), &root, &filepath),
       "#aaa".to_string()
     );
+  }
+
+  #[test]
+  fn test_relative_link() {
+    let root = "/Users/xxx/xxx/xxx/docs".to_string();
+    let filepath = "/Users/xxx/xxx/xxx/docs/zh/guide/config.md".to_string();
+    assert_eq!(
+      normalize_link(&"./guide/config.md".to_string(), &root, &filepath),
+      "/zh/guide/guide/config".to_string()
+    );
+    assert_eq!(
+      normalize_link(&"../guide/config.md".to_string(), &root, &filepath),
+      "/zh/guide/config".to_string()
+    );
+    assert_eq!(
+      normalize_link(&"../../guide/config.md".to_string(), &root, &filepath),
+      "/zh/guide/config".to_string()
+    );
+  }
+
+  #[test]
+  fn test_absolute_link() {
+    let root = "/Users/xxx/xxx/xxx/docs".to_string();
+    let filepath = "/Users/xxx/xxx/xxx/docs/zh/guide/config.md".to_string();
+    assert_eq!(
+      normalize_link(&"/zh/guide/config.md".to_string(), &root, &filepath),
+      "/zh/guide/config".to_string()
+    );
+    assert_eq!(
+      normalize_link(&"/zh/guide/config".to_string(), &root, &filepath),
+      "/zh/guide/config".to_string()
+    );
+  }
+
+  #[test]
+  fn test_img_element() {
+    let root = "/Users/xxx/xxx/xxx/docs".to_string();
+    let filepath = "/Users/xxx/xxx/xxx/docs/zh/guide/config.md".to_string();
+    let mut node = hast::Node::Root(hast::Root {
+      children: vec![hast::Node::MdxJsxElement(hast::MdxJsxElement {
+        name: Some("img".to_string()),
+        attributes: vec![hast::AttributeContent::Property(hast::MdxJsxAttribute {
+          name: "src".to_string(),
+          value: Some(hast::AttributeValue::Literal("../assets/a.png".to_string())),
+        })],
+        children: vec![],
+        position: None,
+      })],
+      position: None,
+    });
+    let links = mdx_plugin_normalize_link(&mut node, &root, &filepath);
+    assert_eq!(links.len(), 0);
+    if let hast::Node::Root(root) = node {
+      assert_eq!(root.children.len(), 2);
+      if let hast::Node::MdxjsEsm(esm) = &root.children[0] {
+        assert_eq!(
+          esm.value,
+          "import image_0 from \"/zh/assets/a.png\";".to_string()
+        );
+      }
+      if let hast::Node::MdxJsxElement(element) = &root.children[1] {
+        assert_eq!(element.name, Some("img".to_string()));
+        if let hast::AttributeContent::Property(property) = &element.attributes[0] {
+          assert_eq!(property.name, "src".to_string());
+          if let Some(hast::AttributeValue::Expression(expression)) = &property.value {
+            assert_eq!(expression.value, "image_0".to_string());
+          }
+        }
+      }
+    }
   }
 }
